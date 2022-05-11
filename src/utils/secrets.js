e49e269d697112fbb46bab69cd752ff41825750c
Desire Kaleba <desirekaleba@gmail.com>
2022-05-11 23:51:26 +0300

    add secrets

---
+++ src/utils/secrets.js
@@ -0,0 +1,31 @@
+require('dotenv/config');
+
+const { logger } = require('./logger');
+
+const {
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME
+} = process.env;
+
+const requiredCredentials = [
+    'DB_HOST',
+    'DB_USER',
+    'DB_PASS',
+    'DB_NAME'
+];
+
+for (const credential of requiredCredentials) {
+    if (!process.env[credential]) {
+        logger.error(`Missing required crendential: ${credential}`);
+        process.exit(1);
+    }
+}
+
+module.exports = {
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME
+};
